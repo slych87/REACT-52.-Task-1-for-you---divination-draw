1a878dd6f0764520de1015abc06be45632aa8d8b
Mateusz Slyszewski <mateuszslyszewski@gmail.com>
2020-06-01 12:02:49 +0200

    practice done

---
+++ practice/app.js
@@ -0,0 +1,26 @@
+class Draw extends React.Component {
+  state = {
+    options: ["1", "2", "3"],
+    option: null,
+
+  }
+
+
+  handleShowOption = () => {
+    const index = Math.floor(Math.random() * this.state.options.length)
+    this.setState({
+      option: this.state.options[index]
+    })
+  }
+  render() {
+
+    return (
+      <div>
+        <button onClick={this.handleShowOption}>Zobacz wróżbę</button>
+        {this.state.option ? <h1>{this.state.option}</h1> : null}
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Draw />, document.getElementById('root'))
